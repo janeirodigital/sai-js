e88842bbd6817314d824f14dc9369453d9cbb0ac
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-05-20 16:50:22 -0500

    remove empty packages

--- packages/sample-client/__tests__/sample-client.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sampleClient = require('..');
-
-describe('sample-client', () => {
-    it('needs tests');
-});
--- packages/sample-server/__tests__/sample-server.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sampleServer = require('..');
-
-describe('sample-server', () => {
-    it('needs tests');
-});
