1af17623ffde1b902b834eca9c6d2eee2529e4de
Angel Araya <angel.araya@janeirodigital.com>
2021-06-02 09:49:38 -0600

    Set builder output type to any

--- packages/namespaces/src/builder.ts
+++ packages/namespaces/src/builder.ts
@@ -1,7 +1,8 @@
 import { DataFactory } from 'n3';
 import { NamedNode } from 'rdf-js';
 
-export function buildNamespace(base: string): unknown {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function buildNamespace(base: string): any {
   const handler = {
     get: (target: { base: string }, property: string): NamedNode => DataFactory.namedNode(target.base + property),
   };
