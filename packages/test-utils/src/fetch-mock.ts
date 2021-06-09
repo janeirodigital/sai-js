f605e1df80d43adb6ac153ee856331af2548d5a8
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-06-09 11:21:31 -0500

    test-util: updated rdfjs types
    Co-authored-by: Maciej Samoraj <samurex@users.noreply.github.com>

--- packages/test-utils/src/fetch-mock.ts
+++ packages/test-utils/src/fetch-mock.ts
@@ -1,9 +1,9 @@
-import { DatasetCore } from 'rdf-js'
+import { DatasetCore } from '@rdfjs/types'
 import { parseTurtle } from 'interop-utils'
 //@ts-ignore
 import * as data from 'data-interoperability-panel'
 
 
 export async function fetch (url: string): Promise<DatasetCore> {
-  return await parseTurtle(data[url], url)
+  return parseTurtle(data[url], url)
 }
