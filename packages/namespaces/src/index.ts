842339ced49af8215d24cc7885b3896d7c9c9d7a
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-06-09 19:27:43 -0500

    namespace: added RDF

--- packages/namespaces/src/index.ts
+++ packages/namespaces/src/index.ts
@@ -3,3 +3,4 @@ import { buildNamespace } from './builder';
 export { buildNamespace } from './builder';
 
 export const INTEROP = buildNamespace('http://www.w3.org/ns/solid/interop#');
+export const RDF = buildNamespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
