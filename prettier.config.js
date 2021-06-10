0ce72952661d54e2c82c9579ef4ff06162d205a5
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-06-10 13:07:27 -0500

    Prettier configuration + pre-commit hook (#9)
    * configured prettier
    
    * pre-commit hook with husky

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  endOfLine: 'lf'
+};
