affcfb6dcd6e639e13900ee4dd08f388af1ae8c9
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-06-15 22:02:59 -0500

    utils: getOneMatchingQuad returns undefined 0 found

--- packages/utils/src/match.ts
+++ packages/utils/src/match.ts
@@ -9,15 +9,22 @@ import { Term, Quad, DatasetCore } from '@rdfjs/types';
  * @param graph
  */
 // eslint-disable-next-line max-len
-export const getOneMatchingQuad = (dataset: DatasetCore, subject?: Term, predicate?: Term, object?: Term, graph?: Term): Quad => {
+export const getOneMatchingQuad = (
+  dataset: DatasetCore,
+  subject?: Term,
+  predicate?: Term,
+  object?: Term,
+  graph?: Term
+): Quad => {
   const matches = dataset.match(subject, predicate, object, graph);
-
-  if (matches.size === 0) {
-    throw new Error('Could not find matching quad in the dataset');
-  } else {
-    return [...matches].shift();
-  }
+  return [...matches].shift();
 };
 
 // eslint-disable-next-line max-len
-export const getAllMatchingQuads = (dataset: DatasetCore, subject?: Term, predicate?: Term, object?: Term, graph?: Term): Array<Quad> => [...dataset.match(subject, predicate, object, graph)];
+export const getAllMatchingQuads = (
+  dataset: DatasetCore,
+  subject?: Term,
+  predicate?: Term,
+  object?: Term,
+  graph?: Term
+): Array<Quad> => [...dataset.match(subject, predicate, object, graph)];
