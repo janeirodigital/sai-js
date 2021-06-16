5742ac315616a40743ceda66eeff08935faa19ea
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-06-16 13:54:36 -0500

    data-model: fixed naming of variable
    Co-authored-by: Maciej Samoraj <samurex@users.noreply.github.com>

--- packages/data-model/test/access-receipt-test.ts
+++ packages/data-model/test/access-receipt-test.ts
@@ -7,26 +7,26 @@ const snippetIri = 'https://auth.alice.example/dd442d1b-bcc7-40e2-bbb9-4abfa7309
 
 describe('build', () => {
   test('should return instance of Access Receipt', async () => {
-    const applicationRegistration = await AccessReceipt.build(snippetIri, factory);
-    expect(applicationRegistration).toBeInstanceOf(AccessReceipt);
+    const accessReceipt = await AccessReceipt.build(snippetIri, factory);
+    expect(accessReceipt).toBeInstanceOf(AccessReceipt);
   });
 
   test('should fetch its data', async () => {
-    const applicationRegistration = await AccessReceipt.build(snippetIri, factory);
-    expect(applicationRegistration.dataset.size).toBeGreaterThan(0);
+    const accessReceipt = await AccessReceipt.build(snippetIri, factory);
+    expect(accessReceipt.dataset.size).toBeGreaterThan(0);
   });
 
   test('should build data grants', async () => {
-    const applicationRegistration = await AccessReceipt.build(snippetIri, factory);
-    expect(applicationRegistration.hasDataGrant.length).toBe(4);
-    for (const grant of applicationRegistration.hasDataGrant) {
+    const accessReceipt = await AccessReceipt.build(snippetIri, factory);
+    expect(accessReceipt.hasDataGrant.length).toBe(4);
+    for (const grant of accessReceipt.hasDataGrant) {
       expect(grant).toBeInstanceOf(DataGrant);
     }
   });
 
   test('should link inheriting grants', async () => {
-    const applicationRegistration = await AccessReceipt.build(snippetIri, factory);
-    for (const grant of applicationRegistration.hasDataGrant) {
+    const accessReceipt = await AccessReceipt.build(snippetIri, factory);
+    for (const grant of accessReceipt.hasDataGrant) {
       if (grant.inheritsFromGrantIri) {
         expect(grant.inheritsFromGrant).toBeInstanceOf(DataGrant);
         expect(grant.inheritsFromGrant.iri).toBe(grant.inheritsFromGrantIri);
