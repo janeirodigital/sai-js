fbf303b0c273ca4fbc7cb1977a08df6509091303
Maciej Samoraj <maciej.samoraj@gmail.com>
2021-07-22 18:44:22 +0200

    data-model: test fix
    Co-authored-by: elf Pavlik <elf-pavlik@hackers4peace.net>

--- packages/data-model/test/data-grant-test.ts
+++ packages/data-model/test/data-grant-test.ts
@@ -3,7 +3,7 @@ import 'jest-rdf';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { fetch } from 'interop-test-utils';
 import { ACL } from 'interop-namespaces';
-import { AccessReceipt, InteropFactory, AbstractDataGrant, DataGrant, RemoteDataGrant } from '../src';
+import { AccessReceipt, InteropFactory, AbstractDataGrant, DataGrant, InheritableRemoteDataGrant } from '../src';
 
 const factory = new InteropFactory(fetch);
 const dataGrantIri = 'https://auth.alice.example/cd247a67-0879-4301-abd0-828f63abb252';
@@ -43,7 +43,7 @@ describe('calculateEffectiveAccessMode', () => {
     const remoteDataGrantIri = 'https://auth.alice.example/a691ee69-97d8-45c0-bb03-8e887b2db806';
     const remoteDataGrant = performchartAccessReceipt.hasDataGrant.find(
       (grant) => grant.iri === remoteDataGrantIri
-    ) as RemoteDataGrant;
+    ) as InheritableRemoteDataGrant;
     const localDataGrantIri = 'https://auth.acme.example/f8064946-cb67-469a-8b28-652fd17090f6';
     const localDataGrant = (await factory.dataGrant(localDataGrantIri, remoteDataGrant)) as DataGrant;
     const accessMode = AbstractDataGrant.calculateEffectiveAccessMode(localDataGrant);
