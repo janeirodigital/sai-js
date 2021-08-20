418fea716dad74e1773da140a9da62b53f6684a3
elf Pavlik <elf-pavlik@hackers4peace.net>
2021-08-19 23:06:52 -0500

    application: clientId is not needed

--- packages/utils/src/link-header.ts
+++ packages/utils/src/link-header.ts
@@ -1,6 +1,5 @@
 import * as LinkHeader from 'http-link-header';
 
-// TODO (elf-pavlik) make stricter by accepting clientId
 export function getApplicationRegistrationIri(linkHeaderText: string): string {
   const links = LinkHeader.parse(linkHeaderText).refs;
   return links.find((link) => link.rel === 'http://www.w3.org/ns/solid/interop#registeredApplication')?.anchor;
