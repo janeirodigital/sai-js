378a3f5b73bde75a4bb4ab81ff1300ce55cff3fd
Maciej Samoraj <maciej.samoraj@gmail.com>
2021-12-08 20:43:45 +0100

    data-model: moved logic from agent registration to resource
    Co-authored-by: elf Pavlik <elf-pavlik@hackers4peace.net>

--- packages/data-model/src/crud/resource.ts
+++ packages/data-model/src/crud/resource.ts
@@ -1,13 +1,15 @@
-import { INTEROP } from '@janeirodigital/interop-namespaces';
+import { INTEROP, XSD } from '@janeirodigital/interop-namespaces';
 import { getOneMatchingQuad } from '@janeirodigital/interop-utils';
 import { DataFactory, Store } from 'n3';
-import { InteropFactory, ReadableResource } from '..';
+import { AuthorizationAgentFactory, InteropFactory, ReadableResource } from '..';
 
 type Data = { [key: string]: string | string[] };
 
 // TODO (elf-pavlik) implement creating new resource
 export class CRUDResource extends ReadableResource {
   data?: Data;
+
+  factory: AuthorizationAgentFactory;
 
   constructor(iri: string, factory: InteropFactory, data?: Data) {
     super(iri, factory);
@@ -28,6 +30,7 @@ export class CRUDResource extends ReadableResource {
    * @throws Error if fails
    */
   public async update(): Promise<void> {
+    this.setTimestampsAndAgents();
     const { ok } = await this.fetch(this.iri, {
       method: 'PUT',
       dataset: this.dataset
@@ -35,6 +38,75 @@ export class CRUDResource extends ReadableResource {
     if (!ok) {
       throw new Error('failed to update');
     }
+  }
+
+  private setTimestampsAndAgents(): void {
+    if (this.data) {
+      this.registeredBy = this.factory.webId;
+      this.registeredWith = this.factory.agentId;
+      this.registeredAt = new Date();
+    }
+    this.updatedAt = new Date();
+  }
+
+  // eslint-disable-next-line consistent-return
+  get registeredAt(): Date | undefined {
+    const object = this.getObject('registeredAt');
+    if (object) {
+      return new Date(object.value);
+    }
+  }
+
+  set registeredAt(date: Date) {
+    this.deleteQuad('registeredAt');
+    this.dataset.add(
+      DataFactory.quad(
+        DataFactory.namedNode(this.iri),
+        INTEROP.registeredAt,
+        DataFactory.literal(date.toISOString(), XSD.dateTime)
+      )
+    );
+  }
+
+  // eslint-disable-next-line consistent-return
+  get updatedAt(): Date | undefined {
+    const object = this.getObject('updatedAt');
+    if (object) {
+      return new Date(object.value);
+    }
+  }
+
+  set updatedAt(date: Date) {
+    this.deleteQuad('updatedAt');
+    this.dataset.add(
+      DataFactory.quad(
+        DataFactory.namedNode(this.iri),
+        INTEROP.updatedAt,
+        DataFactory.literal(date.toISOString(), XSD.dateTime)
+      )
+    );
+  }
+
+  get registeredBy(): string | undefined {
+    return this.getObject('registeredBy')?.value;
+  }
+
+  set registeredBy(agent: string) {
+    this.deleteQuad('registeredBy');
+    this.dataset.add(
+      DataFactory.quad(DataFactory.namedNode(this.iri), INTEROP.registeredBy, DataFactory.literal(agent, XSD.string))
+    );
+  }
+
+  get registeredWith(): string | undefined {
+    return this.getObject('registeredWith')?.value;
+  }
+
+  set registeredWith(agent: string) {
+    this.deleteQuad('registeredWith');
+    this.dataset.add(
+      DataFactory.quad(DataFactory.namedNode(this.iri), INTEROP.registeredWith, DataFactory.literal(agent, XSD.string))
+    );
   }
 
   /*
