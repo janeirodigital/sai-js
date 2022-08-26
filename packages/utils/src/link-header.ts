3053f926c16f8689bbbea0fec0a4a2c0f85a6ca2
Angel Araya <angel.araya@janeirodigital.com>
2022-08-25 20:58:35 -0600

    Explicitely serialize as text/turtle when requested

--- packages/utils/src/link-header.ts
+++ packages/utils/src/link-header.ts
@@ -2,7 +2,7 @@ import LinkHeader from 'http-link-header';
 
 export function getApplicationRegistrationIri(linkHeaderText: string): string {
   const links = LinkHeader.parse(linkHeaderText).refs;
-  return links.find((link) => link.rel === 'http://www.w3.org/ns/solid/interop#registeredApplication')?.anchor;
+  return links.find((link) => link.rel === 'http://www.w3.org/ns/solid/interop#registeredAgent')?.anchor;
 }
 
 export function targetDataRegistrationLink(dataRegistrationIri: string): string {
