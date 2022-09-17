42833fc4f5da3a4a80affd8752a8497bb84ded5e
elf Pavlik <elf-pavlik@hackers4peace.net>
2022-09-16 19:35:23 -0500

    [namespaces] added few more common namespaces

--- packages/namespaces/src/index.ts
+++ packages/namespaces/src/index.ts
@@ -4,7 +4,13 @@ export { buildNamespace } from './builder';
 
 export const INTEROP = buildNamespace('http://www.w3.org/ns/solid/interop#');
 export const RDF = buildNamespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+export const RDFS = buildNamespace('http://www.w3.org/2000/01/rdf-schema#');
 export const LDP = buildNamespace('http://www.w3.org/ns/ldp#');
 export const ACL = buildNamespace('http://www.w3.org/ns/auth/acl#');
 export const SHAPETREES = buildNamespace('http://www.w3.org/ns/shapetrees#');
 export const XSD = buildNamespace('http://www.w3.org/2001/XMLSchema#');
+export const SKOS = buildNamespace('http://www.w3.org/2004/02/skos/core#');
+export const ACP = buildNamespace('http://www.w3.org/ns/solid/acp#');
+export const SOLID = buildNamespace('http://www.w3.org/ns/solid/terms#');
+export const OIDC = buildNamespace('http://www.w3.org/ns/solid/oidc#');
+export const DC = buildNamespace('http://purl.org/dc/terms/');
