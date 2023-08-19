944028061dfe6cc7ffc4a32f5ac2bc4703bffb94
elf Pavlik <elf-pavlik@hackers4peace.net>
2023-08-19 13:49:40 -0600

    [application] initial web sockets support

--- packages/application/src/application.ts
+++ packages/application/src/application.ts
@@ -1,3 +1,4 @@
+import { SubscriptionClient } from '@solid-notifications/subscription';
 import { ApplicationFactory, ReadableApplicationRegistration, DataOwner } from '@janeirodigital/interop-data-model';
 import {
   WhatwgFetch,
@@ -5,12 +6,17 @@ import {
   fetchWrapper,
   discoverAuthorizationAgent,
   discoverAgentRegistration,
-  discoverAuthorizationRedirectEndpoint
+  discoverAuthorizationRedirectEndpoint,
+  NOTIFY
 } from '@janeirodigital/interop-utils';
 
 interface ApplicationDependencies {
   fetch: WhatwgFetch;
   randomUUID(): string;
+}
+
+export interface SaiEvent {
+  type: string;
 }
 
 export class Application {
@@ -20,28 +26,61 @@ export class Application {
 
   fetch: RdfFetch;
 
+  private transformStream: TransformStream;
+
+  private writeableStream: WritableStream<SaiEvent>;
+
+  public stream: ReadableStream<SaiEvent>;
+
   authorizationAgentIri: string;
 
   authorizationRedirectEndpoint: string;
 
+  registrationIri: string;
+
+  // TODO rename
   hasApplicationRegistration?: ReadableApplicationRegistration;
 
   constructor(public webId: string, public applicationId: string, dependencies: ApplicationDependencies) {
     this.rawFetch = dependencies.fetch;
     this.fetch = fetchWrapper(this.rawFetch);
     this.factory = new ApplicationFactory({ fetch: this.fetch, randomUUID: dependencies.randomUUID });
+    this.transformStream = new TransformStream();
+    this.stream = this.transformStream.readable;
+    this.writeableStream = this.transformStream.writable;
   }
 
   private async bootstrap(): Promise<void> {
     this.authorizationAgentIri = await discoverAuthorizationAgent(this.webId, this.fetch);
-    const applicationRegistrationIri = await discoverAgentRegistration(this.authorizationAgentIri, this.rawFetch);
-    if (applicationRegistrationIri) {
-      this.hasApplicationRegistration = await this.factory.readable.applicationRegistration(applicationRegistrationIri);
-    }
+    this.registrationIri = await discoverAgentRegistration(this.authorizationAgentIri, this.rawFetch);
     this.authorizationRedirectEndpoint = await discoverAuthorizationRedirectEndpoint(
       this.authorizationAgentIri,
       this.rawFetch
     );
+    if (!this.registrationIri) return;
+    await this.buildRegistration();
+    await this.subscribeToRegistration();
+  }
+
+  private async buildRegistration(): Promise<void> {
+    if (this.registrationIri) {
+      this.hasApplicationRegistration = await this.factory.readable.applicationRegistration(this.registrationIri);
+    }
+  }
+
+  // TODO: fail gracefully
+  private async subscribeToRegistration(): Promise<void> {
+    const subscriptionClient = new SubscriptionClient(this.fetch);
+    const channel = await subscriptionClient.subscribe(this.registrationIri, NOTIFY.WebSocketChannel2023.value);
+    // TODO: move Web Socket to a Web Worker
+    const websocket = new WebSocket(channel.receiveFrom);
+    websocket.onmessage = async (evt) => {
+      const data = JSON.parse(evt.data);
+      if (data.type === 'Update') {
+        await this.buildRegistration();
+        this.writeableStream.getWriter().write({ type: 'GRANT' });
+      }
+    };
   }
 
   get authorizationRedirectUri(): string {
