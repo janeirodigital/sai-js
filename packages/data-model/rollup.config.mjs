3e38a70db62db0aedcaa2ade71cffbd9f062feb8
elf Pavlik <elf-pavlik@hackers4peace.net>
2024-03-18 11:52:49 -0600

    release 1.0.0-rc.24
    Co-authored-by: Maciej Samoraj <maciej.samoraj@gmail.com>

---
+++ packages/data-model/rollup.config.mjs
@@ -0,0 +1,4 @@
+import createConfig from '@inrupt/base-rollup-config';
+import pkg from "./package.json" assert { type: "json" };
+
+export default createConfig(pkg)
--- packages/service/jest.config.mjs
+++ packages/service/jest.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '/test/.*-test.ts$',
@@ -8,3 +8,5 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   setupFilesAfterEnv: ['./setup-jest.js']
 };
+
+export default config
